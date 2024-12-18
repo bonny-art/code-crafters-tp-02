e2ddfa6407cb2bd5d7b84f86453997be8e98fd87
bonny-art <134740924+bonny-art@users.noreply.github.com>
2024-12-18 16:31:10 +0000

    Deploying to gh-pages from @ bonny-art/code-crafters-tp-02@94b3ec55b4ecb264e653e5b272b2a85cb9edfe5c 🚀

--- assets/reviews-swiper-CDtGDl7l.js
+++
@@ -1,2 +0,0 @@
-import{S as e}from"./vendor-CUnLjt9d.js";new e(".reviews-swiper-container",{grabCursor:!0,breakpoints:{320:{slidesPerView:1,spaceBetween:20},768:{slidesPerView:2,spaceBetween:32},1280:{slidesPerView:3,spaceBetween:32}},loop:!0});
-//# sourceMappingURL=reviews-swiper-CDtGDl7l.js.map
