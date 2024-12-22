2d307d3382249690330b5481933cd97bc6f1ff9e
bonny-art <svitlana.otenko@gmail.com>
2024-12-22 17:12:00 +0200

    Add backdrop scroll block

--- src/js/menu.js
+++ src/js/menu.js
@@ -3,12 +3,22 @@ const mobileMenu = document.querySelector('.mobile-menu-js');
 const backdrop = document.querySelector('.backdrop-js');
 const closeButton = document.querySelector('.mobile-menu-close-js');
 
+const disableScroll = () => {
+  document.body.style.overflow = 'hidden';
+};
+
+const enableScroll = () => {
+  document.body.style.overflow = '';
+};
+
 const closeMenu = () => {
   mobileMenu.style.transform = 'translateX(100%)';
 
   setTimeout(() => {
     backdrop.style.opacity = 0;
     backdrop.style.visibility = 'hidden';
+
+    enableScroll();
   }, 300);
 };
 
@@ -16,6 +26,8 @@ burgerButton.addEventListener('click', () => {
   backdrop.style.visibility = 'visible';
   backdrop.style.opacity = 1;
   mobileMenu.style.transform = 'translateX(0%)';
+
+  disableScroll();
 });
 
 closeButton.addEventListener('click', closeMenu);
